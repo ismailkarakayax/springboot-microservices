7c80370a005cd0891bee1b3afea08124f02afed1
ismailkarakayax <ismlkrky58@gmail.com>
2024-01-25 15:42:15 +0300

    microservices update

--- microservices-new/product-service/src/main/java/com/ismailkarakayax/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ismailkarakayax.productservice.repository;
-
-import com.ismailkarakayax.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
